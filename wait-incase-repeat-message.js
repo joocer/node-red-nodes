247221e3430cbdc81f760c53baf7712df545c93d
Justin Joyce <justin.joyce@joocer.com>
2019-02-03 20:55:07 +0000

    Update wait-incase-repeat-message.js

--- wait-incase-repeat-message.js
+++ wait-incase-repeat-message.js
@@ -2,31 +2,28 @@
 // it was written to run on a motion sensor, rather than turn the light off
 // immediately on deactive, it waits another 30 seconds
 // this isn't just implemented with a in-built trigger because it doesn't 
-// resetting the clock on an arbitrary attribute
+// support resetting the clock on an arbitrary attribute
 
 
 // when there's multiple presence sensors for the same location
 // treat them like a group, send when one is activated, not when each
 if (msg.payload.event == 'activated') {
-    var flag = flow.get(msg.payload.location);
-    if (flag === undefined) {
-        flow.set(msg.payload.location, 'flag');
-        node.status({fill:"yellow",shape:"dot",text:" " });
-        return msg;
-    }
+    flow.set(msg.payload.location, 'flag');
+    node.status({fill:"yellow",shape:"dot",text: msg.payload.location });
+    return msg;
 }
 
 if (msg.payload.event == 'deactivated') {
-    // if undefined, we've already due to set the close message
+    // if undefined, we're not waiting on a deactivate message
     var flag = flow.get(msg.payload.location);
     if (flag === undefined) { return null }
     
     flow.set(msg.payload.location, undefined);
-    node.status({fill:"blue",shape:"dot",text:" " });
+    node.status({fill:"blue",shape:"dot",text: msg.payload.location });
     setTimeout(function(){
         var flag = flow.get(msg.payload.location);
         if (flag === undefined) {
-            node.status({fill:"black",shape:"dot",text:" " });
+            node.status({fill:"black",shape:"dot",text: msg.payload.location });
             node.send(msg);
         }
     }, 30000);
